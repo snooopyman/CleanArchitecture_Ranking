0902010d92af8b25757ef085a2ae10dc88156e92
Snooopyman <armando.snoop@gmail.com>
2025-02-28 17:15:17 +0100

    Initial Commit

---
+++ CleanArchitecture_Ranking/CleanArchitecture_RankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitecture_RankingApp.swift
+//  CleanArchitecture_Ranking
+//
+//  Created by Armando Cáceres on 28/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitecture_RankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanArchitecture_Ranking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CleanArchitecture_Ranking
+//
+//  Created by Armando Cáceres on 28/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
